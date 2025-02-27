9454369e90c3acb56093800b3c1d42f10b7b915e
aaronli16 <liaaron53@gmail.com>
2025-02-26 16:32:51 -0800

    added navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import '../index.css'
+import './styles/navbar.css'
 import fireIcon from '../img/fireicon.png'
+
 function NavBar() {
   return (
     <nav>
-      <Link to="/" className="nav-logo" aria-label="FireTrack Home">
-        <img src={fireIcon} alt="FireTrack Logo" />
-        <span>FireTrack</span>
-      </Link>
+      <div className="nav-left">
+        <Link to="/" className="nav-logo" aria-label="FireTrack Home">
+          <img src={fireIcon} alt="FireTrack Logo" />
+          <span>FireTrack</span>
+        </Link>
+      </div>
+      
+     
+        
+    
+      
+      <div className="nav-right">
       <Link to="/fire-risk">Fire Risk Assessment</Link>
-      <Link to="/community">Community Posts</Link>
-      <Link to="/fundraiser">Fundraisers & Events</Link>
+        <Link to="/community">Community Posts</Link>
+        <Link to="/fundraiser">Fundraisers & Events</Link>
+        <Link to="/login">
+          <button className="get-started-btn">Get Started</button>
+        </Link>
+      </div>
     </nav>
   );
 }
--- src/components/fireTracker.jsx
+++ src/components/fireTracker.jsx
@@ -1,4 +1,4 @@
-// FireTracker.jsx
+
 import React, { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
