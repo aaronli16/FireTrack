1f750b70c4b32b30254a424eb3f001c30e53655a
aaronli16 <liaaron53@gmail.com>
2025-03-15 14:07:02 -0700

    deleted other css fiels

--- src/components/communityBlog.jsx
+++ src/components/communityBlog.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './styles/styles.css';
+import './styles/communityBlog.css';
 import { getDatabase, ref, onValue, set, push } from 'firebase/database';
 
 
